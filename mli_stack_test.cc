2939388c8edbc20343d5c8c317a9d40ed08da174
anclarma <anclarma@student.42.fr>
2022-05-18 00:30:21 +0200

    test stack mli ok

--- mli_stack_test.cc
+++ mli_stack_test.cc
@@ -122,3 +122,107 @@ TEST(MliStackTest, ListCopy)
 	EXPECT_EQ(ft_stck.empty(), std_stck.empty());
 	printSize(ft_stck, std_stck);
 }
+
+template <class FT_STACK, class STD_STACK>
+void	cmp(const FT_STACK &ft_lhs, const FT_STACK &ft_rhs,
+		const STD_STACK &std_lhs, const STD_STACK &std_rhs)
+{
+	EXPECT_EQ(ft_lhs == ft_rhs, std_lhs == std_rhs);
+	EXPECT_EQ(ft_lhs != ft_rhs, std_lhs != std_rhs);
+	EXPECT_EQ(ft_lhs < ft_rhs, std_lhs < std_rhs);
+	EXPECT_EQ(ft_lhs <= ft_rhs, std_lhs <= std_rhs);
+	EXPECT_EQ(ft_lhs > ft_rhs, std_lhs > std_rhs);
+	EXPECT_EQ(ft_lhs >= ft_rhs, std_lhs >= std_rhs);
+}
+
+TEST(MliStackTest, RelationalOpe)
+{
+	ft_container_type	ft_ctnr;
+	std_container_type	std_ctnr;
+
+	ft_ctnr.push_back(21);
+	ft_ctnr.push_back(42);
+	ft_ctnr.push_back(1337);
+	ft_ctnr.push_back(19);
+	ft_ctnr.push_back(0);
+	ft_ctnr.push_back(183792);
+
+	std_ctnr.push_back(21);
+	std_ctnr.push_back(42);
+	std_ctnr.push_back(1337);
+	std_ctnr.push_back(19);
+	std_ctnr.push_back(0);
+	std_ctnr.push_back(183792);
+
+	t_stack_ft	ft_stck(ft_ctnr);
+	t_stack_ft	ft_stck2(ft_ctnr);
+	t_stack_std	std_stck(std_ctnr);
+	t_stack_std	std_stck2(std_ctnr);
+
+	cmp(ft_stck, ft_stck, std_stck, std_stck);  // 0
+	cmp(ft_stck, ft_stck2, std_stck, std_stck2); // 1
+
+	ft_stck2.push(60);
+	ft_stck2.push(61);
+	ft_stck2.push(62);
+	std_stck2.push(60);
+	std_stck2.push(61);
+	std_stck2.push(62);
+
+	cmp(ft_stck, ft_stck2, std_stck, std_stck2); // 2
+	cmp(ft_stck2, ft_stck, std_stck2, std_stck); // 3
+
+	ft_stck.push(42);
+	std_stck.push(42);
+
+	cmp(ft_stck, ft_stck2, std_stck, std_stck2); // 4
+	cmp(ft_stck2, ft_stck, std_stck2, std_stck); // 5
+
+	ft_stck.push(100);
+	std_stck.push(100);
+
+	cmp(ft_stck, ft_stck2, std_stck, std_stck2); // 6
+	cmp(ft_stck2, ft_stck, std_stck2, std_stck); // 7
+}
+
+TEST(MliStackTest, RelationalOpeList)
+{
+	std::list<TESTED_TYPE>	std_ctnr;
+
+	std_ctnr.push_back(21);
+	std_ctnr.push_back(42);
+	std_ctnr.push_back(1337);
+	std_ctnr.push_back(19);
+	std_ctnr.push_back(0);
+	std_ctnr.push_back(183792);
+
+	ft::stack<TESTED_TYPE, std::list<TESTED_TYPE> >		ft_stck(std_ctnr);
+	std::stack<TESTED_TYPE, std::list<TESTED_TYPE> >	std_stck(std_ctnr);
+	ft::stack<TESTED_TYPE, std::list<TESTED_TYPE> >		ft_stck2(std_ctnr);
+	std::stack<TESTED_TYPE, std::list<TESTED_TYPE> >	std_stck2(std_ctnr);
+
+	cmp(ft_stck, ft_stck, std_stck, std_stck);  // 0
+	cmp(ft_stck, ft_stck2, std_stck, std_stck2); // 1
+
+	ft_stck2.push(60);
+	ft_stck2.push(61);
+	ft_stck2.push(62);
+	std_stck2.push(60);
+	std_stck2.push(61);
+	std_stck2.push(62);
+
+	cmp(ft_stck, ft_stck2, std_stck, std_stck2); // 2
+	cmp(ft_stck2, ft_stck, std_stck2, std_stck); // 3
+
+	ft_stck.push(42);
+	std_stck.push(42);
+
+	cmp(ft_stck, ft_stck2, std_stck, std_stck2); // 4
+	cmp(ft_stck2, ft_stck, std_stck2, std_stck); // 5
+
+	ft_stck.push(100);
+	std_stck.push(100);
+
+	cmp(ft_stck, ft_stck2, std_stck, std_stck2); // 6
+	cmp(ft_stck2, ft_stck, std_stck2, std_stck); // 7
+}
